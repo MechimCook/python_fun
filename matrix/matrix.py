86172dfd87a2c549c395b90941ad137f07750c76
Mechim <mechim.cook20@gmail.com>
2020-07-15 13:55:09 -0400

    Matrix V2

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,29 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string):
-        rows = matrix_string.split('\n')
-        self.rows = []
-        self.columns = []
-        self.convert(rows)
+        self.matrix = [[int(i) for i in line.split()] for line in matrix_string.splitlines()]
 
-    def convert(self, rows):
-        for row in rows:
-            elements = row.split(' ')
-            row = []
-
-            if len(self.columns) == 0:
-                [self.columns.append([]) for i in elements]
-
-            column_num = 0
-
-            for element in elements:
-                element = int(element)
-                row.append(element)
-                self.columns[column_num].append(element)
-                column_num += 1
-            self.rows.append(row)
 
     def row(self, index):
-        return self.rows[index-1]
+        return self.matrix[index-1]
 
     def column(self, index):
-        return self.columns[index-1]
+        return [[row[i] for row in self.matrix] for i in range(len(self.matrix[0]))][index-1]
